e700d22d113cb4e6550ad5b6077da10d6df07a72
Ismail Courr <contact@ismailcourr.ml>
2023-02-11 20:25:11 +0100

    Add WebView and style the status bar

--- App.js
+++ App.js
@@ -1,20 +1,11 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar, View } from 'react-native';
+import { WebView } from 'react-native-webview';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <StatusBar backgroundColor={'#333399'}  />
+      <WebView source={{ uri: 'https://improve-pwa.postcare.pages.dev/' }} />
+    </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
