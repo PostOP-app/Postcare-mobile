6c0954a9dc8233f2b5808c850abec00f2630e089
Ismail Courr <contact@ismailcourr.ml>
2023-02-12 02:13:43 +0100

    Create mobile versio link

--- App.js
+++ App.js
@@ -4,8 +4,8 @@ import { WebView } from 'react-native-webview';
 export default function App() {
   return (
     <>
-      <StatusBar backgroundColor={'#333399'}  />
-      <WebView source={{ uri: 'https://improve-pwa.postcare.pages.dev/' }} />
+      <StatusBar backgroundColor={'#333399'} />
+      <WebView source={{ uri: 'https://mobile-version.postcare.pages.dev/' }} />
     </>
   );
 }
